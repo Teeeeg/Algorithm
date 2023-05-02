772fad77b62aa141fcc49945f389c6d1c3deb0ef
taoyupeng <34847171+Teeeeg@users.noreply.github.com>
2023-05-02 18:07:52 +0800

    ignore program.cs

--- Program.cs
+++ Program.cs
@@ -1,4 +1,7 @@
-﻿using AlgorithmAD.Basic;
+﻿var tuple1 = (1, 2);
+var tuple2 = (2, 3);
+var set = new HashSet<(int, int)>();
 
-var slt = new FloodFill733();
-slt.Test();
+set.Add(tuple1);
+set.Add(tuple2);
+System.Console.WriteLine(set.Contains((2, 3)));
