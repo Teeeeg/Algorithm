2c5de5193f5ed2801ad713e4ba8b6a00e9baaa7b
taoyupeng <34847171+Teeeeg@users.noreply.github.com>
2023-06-09 15:12:33 +0800

    Re-orgnized

---
+++ Top150/RemoveElement27.cs
@@ -0,0 +1,33 @@
+﻿using AlgorithmAD.Extension;
+
+namespace AlgorithmAD.Top150;
+
+public class RemoveElement27
+{
+    public int RemoveElement(int[] nums, int val)
+    {
+        int index = 0;
+
+        foreach (var num in nums)
+        {
+            if (num != val)
+            {
+                nums[index] = num;
+                index += 1;
+            }
+        }
+
+        return index + 1;
+    }
+    public void Test()
+    {
+        var nums = new int[] { 1 };
+        var val = 1;
+
+        var res = RemoveElement(nums, val);
+        Console.WriteLine(res);
+
+        nums.Print();
+    }
+
+}
